fbb67ef38db98fb2328c4c2b7e961d8a19ff795e
Andrefcampos <andrecampos.wnr@gmail.com>
2024-09-17 09:44:35 -0300

    Upgrade: edit files

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 if [ ! -f "/var/www/wp-config.php" ]; then
-    cat << EOF > /var/www/wp-config.php
-        <?php
-        define('DB_NAME', $DB_NAME);
-        define('DB_USER', $ADMIN_NAME);
-        define('DB_PASSWORD', $ADMIN_PASSWORD);
-        define('DB_HOST', 'mariadb:3306');
-        define('DB_CHARSET', 'utf8');
-        define('DB_COLLATE', '');
-        $table_prefix = 'wp_';
-        define('WP_DEBUG', false);
+cat << EOF > /var/www/wp-config.php
+    <?php
+    define('DB_NAME', $DB_NAME);
+    define('DB_USER', $ADMIN_NAME);
+    define('DB_PASSWORD', $ADMIN_PASSWORD);
+    define('DB_HOST', 'mariadb:3306');
+    define('DB_CHARSET', 'utf8');
+    define('DB_COLLATE', '');
+    $table_prefix = 'wp_';
+    define('WP_DEBUG', false);
 EOF
 fi
 
